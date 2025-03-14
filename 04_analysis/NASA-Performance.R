271fa86fdc8c59fbf130c6c95c3e6e1531caf042
AmelieKath <ameliereitmayer@gmail.com>
2025-03-14 13:43:01 +1000

    Update NASA-Performance.R

--- 04_analysis/NASA-Performance.R
+++ 04_analysis/NASA-Performance.R
@@ -22,7 +22,7 @@ dtNASAMASExp2 = dtNASAMASExp2[, Scenario := fcase(Scenario == 1, 1.10,
 
 dtNASAMASExp2[, `:=` (
   Scenario = as.factor(Scenario),
-  Timeframe = as.factor(Timeframe),
+  Timepoint = as.factor(Timepoint),
   ID = as.factor(ID),
   MAS.comfort = as.numeric(MAS.comfort),
   MAS.alertness = as.numeric(MAS.alertness),
@@ -46,7 +46,7 @@ summary_QuestExp2 = dtNASAMASExp2[, .(mean_Mental.demand = mean(Mental.demand, n
                                       mean_MAS.alertness = mean(MAS.alertness, na.rm = TRUE),
                                       sd_MAS.alertness = sd(MAS.alertness, na.rm = TRUE),
                                       median_MAS.alertness = median(MAS.alertness, na.rm = TRUE)),
-                                  by = .(Timeframe,Scenario)]
+                                  by = .(Timepoint,Scenario)]
 
 
 by(summary_QuestExp2$median_Success,summary_QuestExp2$Scenario, shapiro.test)
@@ -56,8 +56,8 @@ summary(model)
 
 
 
-(Successmeansd = ggplot(summary_QuestExp2, aes(x = Timeframe, y = mean_Success, group = Scenario, colour = Scenario))+ #summary_QuestExp2[!is_outlier(summary_QuestExp2$mean_Success, coef= 1.5)]
-  geom_line(aes(x = Timeframe, y = mean_Success))+
+(Successmeansd = ggplot(summary_QuestExp2, aes(x = Timepoint, y = mean_Success, group = Scenario, colour = Scenario))+ #summary_QuestExp2[!is_outlier(summary_QuestExp2$mean_Success, coef= 1.5)]
+  geom_line(aes(x = Timepoint, y = mean_Success))+
   geom_ribbon(aes(ymin = mean_Success - sd_Success, ymax = mean_Success + sd_Success, fill = Scenario),alpha = 0.6, linewidth = 0.25)+
   scale_y_continuous(breaks = seq(1,20,1), limits = c(1,20), labels = c("1\nPerfect","","","","","","","","", "10","","","","","","","","","", "20\nFailure"))+
   scale_x_discrete(labels = c("1","","","", "5","","","","", "10"))+
@@ -127,7 +127,7 @@ PVTNBACKNASATCExp2 = PVTNBACKNASATCExp2[, Scenario := fcase(Scenario == 1, 1.10,
                                                             Scenario == 1000, 593)]
 
 PVTNBACKNASATCExp2[, `:=` (               
-  Timeframe = as.factor(Timeframe),
+  Timepoint = as.factor(Timepoint),
   ID = as.factor(ID),
   Means.correct.responses = as.numeric(Means.correct.responses),
   Medians.correct.responses = as.numeric(Medians.correct.responses),
@@ -148,10 +148,10 @@ PVTNBACKNASATCExp2[, `:=` (
 
 PVTNBACKNASATCExp2[, `:=` (
   Scenario = as.numeric(log10(Scenario)), 
-  Timeframe = as.numeric(Timeframe)
+  Timepoint = as.numeric(Timepoint)
 )]
 
-formula = "Success ~ Scenario + I(Scenario^2) + Timeframe + Timeframe:Scenario + (1|ID)"
+formula = "Success ~ Scenario + I(Scenario^2) + Timepoint + Timepoint:Scenario + (1|ID)"
 model = lmer(formula = formula, data = PVTNBACKNASATCExp2)
 summary(model)
 AIC(model)
